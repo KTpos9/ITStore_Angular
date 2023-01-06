5cd9de93461383e64ef776cfea7167437ef1aef5
kittiphop <kittiphopzheng@hotmail.com>
2023-01-06 20:46:20 +0700

    first commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  imports: [
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
+  ]
+})
+export class MaterialModule {}
