b44c3dbaccba87c4fe2c32401f9f1b6e532713a1
kittiphop <kittiphopzheng@hotmail.com>
2023-01-13 20:12:08 +0700

    test the basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './layouts/main/main.component';
+import { HomeComponent } from './pages/shop/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: MainComponent,
+  children: [{
+    path: '',
+    component: HomeComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
