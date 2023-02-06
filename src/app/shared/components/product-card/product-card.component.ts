7f482a0698c005765144d4da7620465d4b720b1e
kittiphop <kittiphopzheng@hotmail.com>
2023-02-06 21:26:25 +0700

    add some code to test the component

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit{
+  @Input()
+  title: string;
+  @Input()
+  desc: string;
+  @Input()
+  price: string;
+  
+  constructor(){
+    this.title = 'hello world';
+    this.desc = 'desc';
+    this.price = '10,000'
+  }
+  ngOnInit(): void {
+
+  }
 
 }
