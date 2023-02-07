cfaf3bc7d5016e1e75b1dc9e99b73a9d466ec37f
kittiphop <kittiphopzheng@hotmail.com>
2023-02-07 12:28:54 +0700

    fixed typo

--- src/app/pages/shop/home/home.component.ts
+++ src/app/pages/shop/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component , OnInit } from '@angular/core';
-import { Product } from 'src/app/models/product.model';
+import { Product } from 'src/app/models/Product.model';
 
 @Component({
   selector: 'app-home',
