91a4cb118624bbb3a747c3ec60a31b8a4fabffa5
kittiphop <kittiphopzheng@hotmail.com>
2023-02-19 15:10:25 +0700

    get cart item data from db using api

--- src/app/pages/shop/cart-page/cart-page.component.ts
+++ src/app/pages/shop/cart-page/cart-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cart } from 'src/app/models/Cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-page.component.scss']
 })
 export class CartPageComponent {
+  cartItems: Cart[] = [];
+  constructor(private _db: CartService){
+
+  }
+  ngOnInit() {
+    this._db.getCart().subscribe({
+      next: (cartItem) => {
+        this.cartItems = cartItem
+        console.log(cartItem)
+    },
+    error: (response) => {
+      console.log(response)
+    }
+  });
+  }
 
 }
+
