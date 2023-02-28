ef43ba5f6cd71877abe3562f0c3d7350d230ef1b
kittiphop <kittiphopzheng@hotmail.com>
2023-02-28 20:37:50 +0700

    add route to product detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './shared/layouts/mainLayout/mainLayout.component'
 import { HomeComponent } from './pages/shop/home/home.component';
 import { CartPageComponent } from './pages/shop/cart-page/cart-page.component';
+import { ProductDetailPageComponent } from './pages/shop/product-detail-page/product-detail-page.component';
 
 const routes: Routes = [{
   path: '',
@@ -12,8 +13,12 @@ const routes: Routes = [{
     component: HomeComponent
   },
   {
+    path: 'product/:id',
+    component: ProductDetailPageComponent
+  },
+  {
     path:'cart',
-    component: CartPageComponent
+    component: CartPageComponent,
   }]
 }];
 
