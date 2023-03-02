e37d5e17d9673760493e5322c9393039336751d1
kittiphop <kittiphopzheng@hotmail.com>
2023-03-02 23:46:54 +0700

    move material imports to materialModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
     MatToolbarModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule {}
