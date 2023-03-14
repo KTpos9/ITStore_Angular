0524451d9e46464a52d9beae77a619eb8f07acba
kittiphop <kittiphopzheng@hotmail.com>
2023-03-14 22:34:18 +0700

    add bg for login & register page

--- src/app/shared/layouts/mainLayout/mainLayout.component.ts
+++ src/app/shared/layouts/mainLayout/mainLayout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./mainLayout.component.scss']
 })
 export class MainLayoutComponent {
-
+  constructor(public router: Router){}
 }
