f6ebed266ed1741c479894bc3d5ee480d7579203
kittiphop <kittiphopzheng@hotmail.com>
2023-03-28 23:30:43 +0700

    add login functionallity(WIP)

--- src/app/pages/shop/login-page/login-page.component.ts
+++ src/app/pages/shop/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { loginResponse } from 'src/app/models/loginResponse.model';
 import { userAuth } from 'src/app/models/UserAuth.model';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -8,19 +11,60 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
-  token: string;
+  token: loginResponse;
+  loginFormGroup: FormGroup;
 
-  constructor(private auth:AuthService){}
+  constructor(private auth: AuthService, private router: Router) {
+    this.initForm();
+  }
 
-  signIn(user: userAuth){
-    this.auth.signIn(user)
-    .subscribe({
-      next: (token) => {
-        this.token = token;
-      }, 
-      complete: () => {
-        localStorage.setItem("token",this.token);
-      }
+  initForm() {
+    this.loginFormGroup = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
     });
+  }
+
+  signIn() {
+    if (this.loginFormGroup.valid) {
+      let user: userAuth = {
+        email: this.loginFormGroup.get('email')?.value,
+        password: this.loginFormGroup.get('password')?.value
+      }
+      this.auth.signIn(user)
+        .subscribe({
+          next: (token) => {
+            this.token = token;
+            console.log(token);
+          },
+          /*the error return HttpErrorResponse object as follow
+            {
+              "headers": {
+                  "normalizedNames": {},
+                  "lazyUpdate": null
+              },
+              "status": 400,
+              "statusText": "OK",
+              "url": "**url**",
+              "ok": false,
+              "name": "HttpErrorResponse",
+              "message": "Http failure response for **url**: 400 OK",
+              "error": {
+                  "message": "Invalid Email or Password",
+                  "token": ""
+              }
+            }
+          */
+          error: (response) => {
+            console.log(response);
+            alert(response.error.message);
+          },
+          complete: () => {
+            localStorage.setItem("token", this.token.token);
+            console.log('token stored!', this.token.token);
+            this.router.navigate(['/']);
+          }
+        });
+    }
   }
 }
