5144fca8d9e6a91da21bab1ee2641206b3e9cc68
kittiphop <kittiphopzheng@hotmail.com>
2023-03-31 20:51:05 +0700

    add profile picture when authed

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  constructor(public auth: AuthService) {}
 }
