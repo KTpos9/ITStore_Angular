779d111a01544e073ff1e7dd2517a0c24d50b5bf
kittiphop <kittiphopzheng@hotmail.com>
2023-04-06 22:06:25 +0700

    remove unused import

--- src/app/pages/shop/login-page/login-page.component.ts
+++ src/app/pages/shop/login-page/login-page.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { loginResponse } from 'src/app/models/loginResponse.model';
 import { userAuth } from 'src/app/models/UserAuth.model';
 import { AuthService } from 'src/app/services/auth.service';
