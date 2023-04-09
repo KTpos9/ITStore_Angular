ab30226bfedc04cb2d29460da1ab2fadd99a0645
Pakawat <pakawatices@gmail.com>
2023-04-09 20:29:45 +0700

    finishing admin layout

--- src/app/shared/layouts/admin-layout/admin-layout.component.ts
+++ src/app/shared/layouts/admin-layout/admin-layout.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent {
+  isSideNavCollapsed = false;
+  collapsed = false;
+  screenWidth = 0;
+
+  onToggleSideNav(data: SideNavToggle): void{
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
+
+  getBodyClass(): string {
+    let styleClass = '';
+    if(this.collapsed && this.screenWidth > 768){
+      styleClass = 'body-trimmed'
+    } else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth >0){
+      styleClass = 'body-md-screen'
+    }
+    return styleClass;
+  }
 
 }
