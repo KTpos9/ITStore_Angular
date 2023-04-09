0451f91ab255d17c8c1bc86f3b2f22736a5dbf61
Pakawat <pakawatices@gmail.com>
2023-04-09 22:52:47 +0700

    finishing order service

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Order } from '../models/Order.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  //inject Httpclient for calling the api
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  //call the api to get the order as an Observable[]
+  getOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(`${environment.baseApiUrl}/api/Order`);
+  }
 }
