a6770acbcc0a6666f6ac52f01e28912363c06043
Pakawat <pakawatices@gmail.com>
2023-04-09 21:34:42 +0700

    finishing add-product

--- src/app/pages/admin/product-list/add-product/add-product.component.ts
+++ src/app/pages/admin/product-list/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/Product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-add-product',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent {
+  newProduct: Product;
+  categories: string[] = ['GPU', 'CPU', 'Monitor', 'RAM', 'SSD', 'MAINBOARD', 'etc.'];
 
+  constructor(private productService: ProductsService) {
+    this.newProduct = {} as Product;
+  }
+
+  addProduct() {
+    this.productService.addProduct(this.newProduct).subscribe(
+      (response) => {
+        console.log("Product add successfully:",response);
+      },
+      (error) => {
+        console.error("Error:",error);
+      }
+    );
+  }
 }
