9151d19d7ea8df492af0194f690cd06eefd146db
Pakawat <pakawatices@gmail.com>
2023-04-09 22:49:57 +0700

    finishing add-member

--- src/app/pages/admin/member-list/add-member/add-member.component.ts
+++ src/app/pages/admin/member-list/add-member/add-member.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Member } from 'src/app/models/Member.model';
+import { MemberService } from 'src/app/services/member.service';
 
 @Component({
   selector: 'app-add-member',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-member.component.scss']
 })
 export class AddMemberComponent {
+  newMember: Member;
+  Roles: string[] = ['Administrator', 'User'];
 
+  constructor(private memberService: MemberService) {
+    this.newMember = {} as Member;
+  }
+
+  addMember() {
+    this.memberService.addMember(this.newMember).subscribe(
+      (response) => {
+        console.log("Member add successfully:",response);
+      },
+      (error) => {
+        console.error("Error:",error);
+      }
+    );
+  }
 }
