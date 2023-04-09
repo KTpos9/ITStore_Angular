b13dedb1b2567cd0dab233cd13472ac41020a3df
Pakawat <pakawatices@gmail.com>
2023-04-09 22:27:55 +0700

    finishing member service

--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Member } from '../models/Member.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
+  members: Member[];
+  //inject Httpclient for calling the api
+  constructor(private http: HttpClient) {
+    this.getMembers().subscribe({
+      next: (members) => {
+        this.members = members
+        console.log(members);
+      },
+      error: (response) => {
+        console.log(response);
+        console.log(this.members);
+      }
+    });
+  }
 
-  constructor() { }
+  //call the api to get the product as an Observable[]
+  getMembers(): Observable<Member[]>{
+    return this.http.get<Member[]>(`${environment.baseApiUrl}/api/Member`);
+  }
+
+  addMember(member: Member): Observable<Member>{
+    return this.http.post<Member>(`${environment.baseApiUrl}/api/Member`, member);
+  }
+
+  updateMember(member: Member): Observable<Member>{
+    return this.http.put<Member>(`${environment.baseApiUrl}/api/Member/${member.MemberId}`, member);
+  }
+
+  deleteMember(MemberId: number): Observable<void> {
+    return this.http.delete<void>(`${environment.baseApiUrl}/api/Member/${MemberId}`);
+  }
 }
